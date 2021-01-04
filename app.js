1975e4823acaa1e968ed0fb5caf145f36ede0f71
Frankie116 <frankbotchey@hotmail.com>
2021-01-04 07:25:54 +0000

    Updated app.js to trigger codepipeline deployment...

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Fargate test successfully completed again!"));
+app.get("/", (req, res) => res.send("Fargate test successfully completed."));
 
 app.get("/health", (req, res) => {
   res.status(200);
